426c6a48aca8ba9a7a43ce332777f291d33890d9
Daniel Boyle <boylede+github@gmail.com>
2021-05-01 19:04:21 -0700

    tweak tests

--- nesse_emu/src/test.rs
+++ nesse_emu/src/test.rs
@@ -1,16 +1,15 @@
 use super::*;
 
 #[test]
-fn test_0xa9_lda_immidiate_load_data() {
+fn test_0xa9_lda_immediate_load_data() {
     let mut nes = Nes::default();
     nes.inject_operation("a9 05 00");
     nes.step();
     let regs = nes.dump_registers();
-    let mut expected = NesRegisters::default();
-    expected.set_a(5);
-    println!("result: {:?}", regs);
-    assert!(regs.a == expected.a);
-    assert!(regs.p == expected.p);
+    let expected = NesRegisters::default().with_a(5).with_pc(2);
+    println!("result: {:?}\nexpect: {:?}", regs, expected);
+    assert!(regs == expected);
+    assert!(regs == expected);
 }
 
 #[test]
@@ -20,7 +19,10 @@ fn test_0xa9_lda_zero_flag() {
     nes.inject_operation("a9 00 00");
     nes.step();
     let regs = nes.dump_registers();
-    let expected = NesRegisters::default().with_flags_from(-2i8 as u8).with_flags_from(0).with_pc(1);
+    let expected = NesRegisters::default()
+        .with_flags_from(-2i8 as u8)
+        .with_flags_from(0)
+        .with_pc(1);
     println!("result: {:?}\nexpect: {:?}", regs, expected);
     assert!(regs.status_zero() == expected.status_zero());
 }
@@ -32,10 +34,13 @@ fn test_0xaa_tax_move_a_to_x() {
     nes.inject_operation("aa 00");
     nes.step();
     let regs = nes.dump_registers();
-    let expected = NesRegisters::default().with_x(10).with_flags_from(10);
-    println!("result: {:?}\n{:?}", regs, expected);
-    assert!(regs.x == expected.x);
-    assert!(regs.p & 0b10 == expected.p & 0b10);
+    let expected = NesRegisters::default()
+        .with_a(10)
+        .with_x(10)
+        .with_flags_from(10)
+        .with_pc(1);
+    println!("result: {:?}\nexpect: {:?}", regs, expected);
+    assert!(regs == expected);
 }
 
 #[test]
@@ -45,11 +50,11 @@ fn test_5_ops_working_together() {
     nes.run_until_nop();
     let regs = nes.dump_registers();
     let expected = NesRegisters::default()
-        .with_x(0xc1)
-        .with_a(192)
-        .with_flags_from(0xe8)
+        .with_x(0xc0 + 1)
+        .with_a(0xc0)
+        .with_flags_from(0xc0 + 1)
         .with_pc(4);
-    println!("result: {:?}\n{:?}", regs, expected);
+    println!("result: {:?}\nexpect: {:?}", regs, expected);
     assert!(regs == expected);
 }
 
@@ -64,6 +69,6 @@ fn test_inx_overflow() {
         .with_x(0x1)
         .with_flags_from(0x1)
         .with_pc(2);
-    println!("result: {:?}\n{:?}", regs, expected);
+    println!("result: {:?}\nexpect: {:?}", regs, expected);
     assert!(regs == expected);
 }
